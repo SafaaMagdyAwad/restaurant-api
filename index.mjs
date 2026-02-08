c4dd9fd859f52e1e287c21b50796fe0d81eec66e
Safaa Magdy <engsafaamagdy1024@gmail.com>
2026-02-08 21:34:28 +0200

    c

--- index.mjs
+++ index.mjs
@@ -17,10 +17,27 @@ dotenv.config();
 const app = express();
 
 // ================= Middleware =================
+const allowedOrigins = [
+  "https://resturant-five-lake.vercel.app"
+];
+
 app.use(cors({
-  origin: ["https://resturant-five-lake.vercel.app"],
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // requests من Postman / server
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 }));
+
+// مهم جدًا
+app.options("*", cors());
+
 app.use(express.json());
 
 // ================= Test Route =================
